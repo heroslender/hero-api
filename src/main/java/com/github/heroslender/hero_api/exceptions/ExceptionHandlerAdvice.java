d9c40c9d73d3a9bc123fdd20abefbf227094a370
heroslender <admin@heroslender.com>
2025-11-21 22:56:32 +0000

    Catch unhandled exceptions

--- src/main/java/com/github/heroslender/hero_api/exceptions/ExceptionHandlerAdvice.java
+++ src/main/java/com/github/heroslender/hero_api/exceptions/ExceptionHandlerAdvice.java
@@ -1,5 +1,8 @@
 package com.github.heroslender.hero_api.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +14,7 @@ import java.util.Date;
 
 @RestControllerAdvice
 class ExceptionHandlerAdvice {
+    private final Logger log = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -36,6 +40,13 @@ class ExceptionHandlerAdvice {
         return toDto(ex);
     }
 
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    ApiExceptionDTO exHandler(Exception ex) {
+        log.error("Unhandled exception!", ex);
+        return new ApiExceptionDTO((short) 500, formatTimestamp(System.currentTimeMillis()), "Internal Server Error");
+    }
+
     private ApiExceptionDTO toDto(RestApiExcetion ex) {
         return new ApiExceptionDTO(
                 ex.getStatusCode(),
@@ -51,11 +62,16 @@ class ExceptionHandlerAdvice {
         return format.format(date);
     }
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private record ApiExceptionDTO(
             short code,
             String timestamp,
             String error,
             String message
     ) {
+
+        private ApiExceptionDTO(short code, String timestamp, String error) {
+            this(code, timestamp, error, null);
+        }
     }
 }
