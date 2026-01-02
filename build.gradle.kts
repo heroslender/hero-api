5eb8127def832a7ca1771c697347eeabde355b1e
heroslender <bruno.martins1998@hotmail.com>
2026-01-02 19:54:48 +0000

    Add OpenAPI for api docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.15")
+
     implementation("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
